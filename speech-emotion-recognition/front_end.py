5090b1e9413d0e8fb204b31b58145355c9f74356
Hadrien Rose <hadrien.rose@protonmail.com>
2022-03-06 05:25:06 +0000

    Beginning the Front_End_Develpement

---
+++ speech-emotion-recognition/front_end.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+import numpy as np
+import pandas as pd
+
+st.markdown("<h1 style='text-align: center; color: red;'>Record Your Emotions</h1>", unsafe_allow_html=True )
+#background image
+
+if st.button('Record', help='record your emotions'):
+     st.write('Work in development 🚧')
