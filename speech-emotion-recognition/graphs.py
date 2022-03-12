5c0af7180e9b42c4dc5a5ec670feec81874a8d86
George Wilson <wilson-george@hotmail.com>
2022-03-12 10:57:33 +0000

    front end stuff

--- speech-emotion-recognition/graphs.py
+++ speech-emotion-recognition/graphs.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import librosa
 import librosa.display
 
+from predict import chunks, model_predict
+
 def draw_mel(uploaded_file):
     sr = 44100
     file = librosa.load(uploaded_file, sr=sr)
@@ -15,3 +17,10 @@ def draw_mel(uploaded_file):
     ax.set(title='Now with labeled axes!')
     fig.colorbar(img, ax=ax, format="%+2.f dB")
     return fig
+
+def plot_chunks(file):
+    chunks = chunks(file)
+    chunk_preds = []
+    for chunk in chunks:
+        chunk_pred = model_predict(chunk)
+        chunk_preds.append(chunk_pred)
