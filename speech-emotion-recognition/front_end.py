f6de35f9eeb604a4b5ab5dac1b4d4c7aaf2bedd0
Julian Boecker <jrboecker@outlook.com>
2022-03-12 13:51:05 +0000

    changes

--- speech-emotion-recognition/front_end.py
+++ speech-emotion-recognition/front_end.py
@@ -56,22 +56,11 @@ st.header("1. Record your own voice")
 
 
 if st.button(f"Click to Record"):
-<<<<<<< HEAD
     record_state = st.text("Recording...")
     duration = 5  # seconds
     fs = 48000
     myrecording = record(duration, fs)
     record_state.text(f"Saving sample as test.wav")
-=======
-    if filename == "":
-        st.warning("Choose a filename.")
-    else:
-        record_state = st.text("Recording...")
-        duration = 5  # seconds
-        fs = 44100
-        myrecording = record(duration, fs)
-        record_state.text(f"Saving sample as {filename}.mp3")
->>>>>>> cd99e255247517da6509edbfb30d0c2b6e608ea4
 
     uploaded_file = f"./temporary_recording/test.wav"
 
