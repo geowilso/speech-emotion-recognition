fa8a1b1d4ba4ccd7e40a4be9323799a3e674f4ef
Julian Boecker <jrboecker@outlook.com>
2022-03-12 11:09:08 +0000

    recording2

--- speech-emotion-recognition/front_end copy_julian.py
+++ speech-emotion-recognition/front_end copy_julian.py
@@ -1,24 +1,25 @@
-import numpy as np
-import sounddevice as sd
-import wave
+import streamlit as st
+import soundfile as sf
+from helper import read_audio, record, save_record
+"# Streamlit showcase"
 
-FILE_NAME = './test.wav'  #File name to save
-wave_length = 5  #Recording length (seconds)
-sample_rate = 44100 #Sampling frequency
+st.header("1. Record your own voice")
 
-#Start recording (wave_length Record for seconds. Wait until the recording is finished with wait)
-data = sd.rec(int(wave_length * sample_rate), sample_rate, channels=1)
-sd.wait()
+filename = st.text_input("Choose a filename: ")
 
-#Normalize. Since it is recorded with 16 bits of quantization bit, it is maximized in the range of int16.
-data = data / data.max() * np.iinfo(np.int16).max
+if st.button(f"Click to Record"):
+    if filename == "":
+        st.warning("Choose a filename.")
+    else:
+        record_state = st.text("Recording...")
+        duration = 5  # seconds
+        fs = 48000
+        myrecording = record(duration, fs)
+        record_state.text(f"Saving sample as {filename}.mp3")
 
-# float -> int
-data = data.astype(np.int16)
+        path_myrecording = f"./samples/{filename}.mp3"
 
-#Save file
-with wave.open(FILE_NAME, mode='wb') as wb:
-    wb.setnchannels(1)  #monaural
-    wb.setsampwidth(2)  # 16bit=2byte
-    wb.setframerate(sample_rate)
-    wb.writeframes(data.tobytes())  #Convert to byte string
+        save_record(path_myrecording, myrecording, fs)
+        record_state.text(f"Done! Saved sample as {filename}.mp3")
+
+        st.audio(read_audio(path_myrecording))
