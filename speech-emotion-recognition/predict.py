3c4d2c10f63654a965e57054eb76688b69a96aa5
George Wilson <wilson-george@hotmail.com>
2022-03-16 10:52:24 +0000

    packages

--- speech-emotion-recognition/predict.py
+++ speech-emotion-recognition/predict.py
@@ -104,6 +104,57 @@ def grab_chunks(uploaded_file):
     return df,wav
 
 
+def grab_chunks_uploaded(uploaded_file):
+    n_mfcc = 13
+    n_fft = 2048
+    hop_length = 512
+
+    wav = librosa.load(uploaded_file, sr=44100)
+
+    sr = wav[1]
+
+    mfcc = librosa.feature.mfcc(wav[0],
+                                sr=sr,
+                                n_mfcc=n_mfcc,
+                                n_fft=n_fft,
+                                hop_length=hop_length)
+
+    mfcc_T = mfcc.T
+
+    mfcc_list = []
+
+    while len(mfcc_T) > 125:
+        mfcc_chunk = mfcc_T[:125, :]
+        mfcc_list.append(mfcc_chunk)
+        mfcc_T = mfcc_T[125:, :]
+    mfcc_list.append(mfcc_T)
+
+    mfcc_list_pad = []
+
+    for i in mfcc_list:
+        mfcc = i.T
+        mfcc_pad = pad_sequences(mfcc,
+                                 maxlen=615,
+                                 dtype='float32',
+                                 padding='post',
+                                 value=-1000.)
+        mfcc_pad_T = mfcc_pad.T
+        mfcc_pad_T_reshape = mfcc_pad_T.reshape(1, 615, 13)
+        mfcc_list_pad.append(mfcc_pad_T_reshape)
+
+    model = load_model("models/speech_emotion_model_0.h5")
+
+    predictions = []
+
+    for i in mfcc_list_pad:
+        prediction = model.predict(i)
+        predictions.append(prediction[0])
+
+    array = np.array(predictions)
+    df = pd.DataFrame(array, columns=['Angry', 'Happy', 'Neutral', 'Sad'])
+
+    return df, wav
+
 def model_predict(mfcc_pad_T_reshape):
     model = load_model("models/speech_emotion_model_0.h5")
     results = model.predict(mfcc_pad_T_reshape)
